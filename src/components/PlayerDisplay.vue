7974f3a8a4aed85cee1acecd4d3364a7231e66d9
Julia <leoherbsthofer@icloud.com>
2021-04-16 23:43:13 +0200

    Fixed color swatch positioning

--- src/components/PlayerDisplay.vue
+++ src/components/PlayerDisplay.vue
@@ -2,11 +2,12 @@
     <tr>
         <td>{{player.id}}</td>
         <td><input type="text" solo v-model="player.content"></td>
-        <td class="swatch">
-            <v-swatches solo v-model="player.backgroundColor"></v-swatches>
+        <td>
+            <v-swatches class="swatch" background-color="#212121" solo v-model="player.backgroundColor"></v-swatches>
         </td>
         <td>
-            <input type="color" solo v-model="player.foregroundColor"/>
+            <v-swatches class="swatch" background-color="#212121" solo v-model="player.foregroundColor"></v-swatches>
+            <!--<input type="color" solo v-model="player.foregroundColor"/>-->
         </td>
         <td><input type="text" solo v-model="player.size"></td>
         <v-btn icon color="pink" @click.native="deletePlayer()">
@@ -22,6 +23,9 @@ import 'vue-swatches/dist/vue-swatches.css'
 export default {
     components: {VSwatches},
     name: 'PlayerDisplay',
+    mounted() {
+        console.log(this.$vuetify.theme.themes.dark);
+    },
     props: {
         player: Object,
         data() {
@@ -37,12 +41,25 @@ export default {
 </script>
 
 <style scoped>
+tr {
+    max-height: 100px;
+}
+
 input {
     color: white;
     width: 50%;
+    text-align: center;
 }
 
-.swatch {
-    position: absolute;
+.swatch >>> .vue-swatches__container {
+    position: fixed;
+    left: 50% !important;
+    top: 50% !important;
+    transform: translate(-50%, -50%);
+ }
+
+ 
+.swatch >>> .vue-swatches__trigger__wrapper {
+    margin-top: 5px;
  }
 </style>
